9cf694c5c2791e61390e0717c897e016ed78555f
Denis Panyushin <Panyushin.D@yandex.ru>
2022-07-29 00:49:54 +0300

    feat: routes, controllers, models

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-unused-vars */
 const Card = require('../models/card');
 
+const VALLIDATE_ERROR_CODE = 400;
+const NOT_FOUND_ERROR_CODE = 404;
+const DEFAULT_ERROR_CODE = 500;
+
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
@@ -12,16 +16,16 @@ module.exports.createCard = (req, res) => {
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
     // eslint-disable-next-line no-unused-vars
-    .catch((err) => res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => res.status(VALLIDATE_ERROR_CODE).send({ message: 'Переданы некорректные данные при создании карточки.' }))
+    .catch((err) => res.status(DEFAULT_ERROR_CODE).send({ message: err.message }));
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => res.send({ data: card }))
     // eslint-disable-next-line no-unused-vars
-    .catch((err) => res.status(404).send({ message: `Карточка с указаным id:${req.params._id} не найдена` }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => res.status(NOT_FOUND_ERROR_CODE).send({ message: `Карточка с указаным id:${req.params._id} не найдена` }))
+    .catch((err) => res.status(DEFAULT_ERROR_CODE).send({ message: err.message }));
 };
 
 module.exports.likeCard = (req, res) => {
@@ -31,9 +35,9 @@ module.exports.likeCard = (req, res) => {
     { new: true },
   )
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка.' }))
-    .catch((err) => res.status(404).send({ message: `Передан несущетвующий id:${req.params._id} карточки` }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => res.status(VALLIDATE_ERROR_CODE).send({ message: 'Переданы некорректные данные для постановки лайка.' }))
+    .catch((err) => res.status(NOT_FOUND_ERROR_CODE).send({ message: `Передан несущетвующий id:${req.params._id} карточки` }))
+    .catch((err) => res.status(DEFAULT_ERROR_CODE).send({ message: err.message }));
 };
 
 module.exports.dislikeCard = (req, res) => {
@@ -43,7 +47,7 @@ module.exports.dislikeCard = (req, res) => {
     { new: true },
   )
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(400).send({ message: 'Переданы некорректные данные для снятия лайка.' }))
-    .catch((err) => res.status(404).send({ message: `Передан несущетвующий id:${req.params._id} карточки` }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => res.status(VALLIDATE_ERROR_CODE).send({ message: 'Переданы некорректные данные для снятия лайка.' }))
+    .catch((err) => res.status(NOT_FOUND_ERROR_CODE).send({ message: `Передан несущетвующий id:${req.params._id} карточки` }))
+    .catch((err) => res.status(DEFAULT_ERROR_CODE).send({ message: err.message }));
 };
