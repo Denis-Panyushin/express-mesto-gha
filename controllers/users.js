4fdaf623a08bba5526f40b59fed52140df8db5e7
Denis Panyushin <Panyushin.D@yandex.ru>
2022-08-18 18:09:20 +0300

    feat: router authorization, fix: controllers user, middleware auth, app

--- controllers/users.js
+++ controllers/users.js
@@ -56,6 +56,7 @@ module.exports.postUser = (req, res, next) => {
   const {
     name, about, avatar, email, password,
   } = req.body;
+
   bcrypt.hash(password, 10)
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
@@ -79,6 +80,7 @@ module.exports.postUser = (req, res, next) => {
 
 module.exports.updateUser = (req, res, next) => {
   const { name, about } = req.body;
+
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .orFail(() => {
       const error = new Error();
@@ -98,6 +100,7 @@ module.exports.updateUser = (req, res, next) => {
 
 module.exports.updateAvatar = async (req, res, next) => {
   const { avatar } = req.body;
+
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .orFail(() => {
       const error = new Error();
